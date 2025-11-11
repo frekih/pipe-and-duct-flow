52ad0cede78e4a30ef75a04de7dfde6fdafd9c51
Fredrik Kihlberg <fredrik.kihlberg@norconsult.com>
2025-11-11 15:14:22 +0100

    v0.3

--- frictionFactor.py
+++ frictionFactor.py
@@ -8,11 +8,18 @@ Created on Tue Oct 28 08:42:05 2025
 import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
-from flowProperties import u, d, mu, rho
+from flowProperties import q, d, mu, rho
 
-t0 = 0
-t1 = 100
-dt = 0.1
+f0 = 1
+f1 = 101
+df = 0.1
+
+u = q / (3600 * np.pi * (d/2)**2)
+
+N_sim = int((f1 - f0) / df) + 1
+
+t_array = np.zeros(N_sim)
+f_array = np.zeros(N_sim)
 
 for i in np.arange(0.001, 0.05, 0.001):
     epsilon = i
@@ -21,13 +28,25 @@ r = epsilon / d # roughness [-]
 v = mu / rho # kinematic viscosity [m2/s]
 Re = u * d / v
 
-def f(x):
-    if Re < 4000:
-        x = 64 / Re # for laminar flow
-    else:
-        return (-2 * np.log10((epsilon / 3.7 * d) + (2.51 / Re * np.sqrt(x))) - (1 / np.sqrt(x)))
+for k in range(0, N_sim):
     
-sol = fsolve(f, 0.001)
-print(sol)
+    dt = df * k
+    
+    def f(x):
+        if Re < 4000:
+            x = 64 / Re * dt
+        else:
+            return (-2 * np.log10((r / 3.7) + (2.51 / (Re * np.sqrt(x)))) - (1 / np.sqrt(x)))
+        
+    sol = fsolve(f, 0.001)
+    ff = sol * dt
+        
+    f_array[k] = ff
+    t_array[k] = dt
     
 plt.Figure(figsize=[16, 12])
+plt.plot(t_array, f_array, 'y', label='ff')
+plt.xlim(0, 1)
+plt.ylim(0.4, 0)
+plt.grid()
+plt.show()
