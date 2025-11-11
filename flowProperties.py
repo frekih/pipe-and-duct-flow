484b238c580e942f38326779154c58b7ae5c871e
Fredrik Kihlberg <fredrik.kihlberg@norconsult.com>
2025-11-11 15:15:07 +0100

    v0.2

--- flowProperties.py
+++ flowProperties.py
@@ -5,5 +5,8 @@ Created on Wed Oct 29 10:39:12 2025
 @author: frekih
 """
 
-u = 0.1 # velocity [m/s]
+q = 750 # velocity [m3/h]
 d = 0.1 # diameter [m]
+
+mu = 16.8e-6 # dynamic viscosity [kg/m*s]
+rho = 1.2 # density [kg/m3]
